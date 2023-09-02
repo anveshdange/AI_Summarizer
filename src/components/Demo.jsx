b6d7e2a27dc5e124c796975765860d62f6810b21
anveshdange <dangeanv@gmail.com>
2023-09-02 11:11:35 +0530

    Initial Commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import {copy, linkIcon, loader, tick} from "../assets"
+
+const Demo = () => {
+  return (
+    <div>Demo</div>
+  )
+}
+
+export default Demo
